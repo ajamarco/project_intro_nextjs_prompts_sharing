40ec048b9540bfce748d0d1d69eb5d470698f894
Alexandre Jamarco <a.jamarco@gmail.com>
2024-01-28 12:47:37 +0000

    coded frontend part of creating a new prompt

---
+++ app/create-prompt/page.js
@@ -0,0 +1,41 @@
+"use client";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+//components
+import Form from "@components/Form";
+
+const CreatePrompt = () => {
+  //create a state called submitting to manage the form submission. Also, create a state called post to manage the post data
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+
+  //create a function called createPrompt to handle the form submission
+  const createPrompt = async (e) => {
+    e.preventDefault();
+
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log("something happened... ", error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return <Form type="Create" post={post} setPost={setPost} submitting={submitting} handleSubmit={createPrompt} />;
+};
+
+export default CreatePrompt;
--- components/Form.js
+++ components/Form.js
@@ -1,7 +1,44 @@
-import React from "react";
+import Link from "next/link";
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+  return (
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">{type} and share your prompt with the whole world, helping with the creation of new AI prompts.</p>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5 mt-10 w-full max-w-2xl glassmorphism">
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">Your Prompt here</span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Type your prompt here..."
+            required
+            className="form_textarea"
+          ></textarea>
+        </label>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            tag
+            <span className="font-normal"> (#product, #coding, #idea, #cooking)</span>
+          </span>
+          <input value={post.tag} onChange={(e) => setPost({ ...post, tag: e.target.value })} placeholder="Type your tags in here" required className="form_input"></input>
+        </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className="text-gray-500 text-sm">
+            <button type="button" className="outline_btn">
+              Cancel
+            </button>
+          </Link>
+          <button type="submit" className="px-8 py-2 bg-primary-orange rounded-full text-white " disabled={submitting}>
+            {submitting ? "Submitting..." : type}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default Form;
