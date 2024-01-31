51a9c16729db47d1cccca4135bd551d8a86186c6
Alexandre Jamarco <a.jamarco@gmail.com>
2024-01-31 10:00:49 +0000

    Add Suspense to UpdatePrompt component

--- app/update-prompt/page.js
+++ app/update-prompt/page.js
@@ -1,13 +1,12 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 //components
 import Form from "@components/Form";
-import { get } from "mongoose";
 
 const UpdatePrompt = () => {
   //get the router and session
-  // const router = useRouter();
+  const router = useRouter();
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
 
@@ -16,18 +15,18 @@ const UpdatePrompt = () => {
   const [post, setPost] = useState({ prompt: "", tag: "" });
 
   //useEffect
-  // useEffect(() => {
-  //   const getPromptDetails = async () => {
-  //     const response = await fetch(`/api/prompt/${promptId}`);
-  //     const data = await response.json();
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
 
-  //     setPost({
-  //       prompt: data.prompt,
-  //       tag: data.tag,
-  //     });
-  //   };
-  //   if (promptId) getPromptDetails();
-  // }, [promptId]);
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+    if (promptId) getPromptDetails();
+  }, [promptId]);
 
   //   create a function called createPrompt to handle the form submission
   const updatePrompt = async (e) => {
@@ -45,8 +44,8 @@ const UpdatePrompt = () => {
         }),
       });
       if (response.ok) {
-        // router.push("/");
-        console.log("prompt updated");
+        router.push("/");
+        // console.log("prompt updated");
       }
     } catch (error) {
       console.log("something happened... ", error);
@@ -55,7 +54,11 @@ const UpdatePrompt = () => {
     }
   };
 
-  return <Form type="Edit" post={post} setPost={setPost} submitting={submitting} handleSubmit={updatePrompt} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Form type="Edit" post={post} setPost={setPost} submitting={submitting} handleSubmit={updatePrompt} />
+    </Suspense>
+  );
 };
 
 export default UpdatePrompt;
