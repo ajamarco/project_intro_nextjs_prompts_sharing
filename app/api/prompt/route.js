12f6dc6b9bf1a6e79ae17c720e11d37c93226dff
Alexandre Jamarco <a.jamarco@gmail.com>
2024-01-31 11:27:16 +0000

    Add Cache-Control header to GET response in prompt route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,7 @@ export const GET = async (req, res) => {
   try {
     await connectToDatabase();
     const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompts), { status: 200, headers: { "Cache-Control": "no-store, max-age=0" } });
   } catch (error) {
     console.log("something happened... ", error);
     return new Response("failed to get prompts", { status: 500 });
